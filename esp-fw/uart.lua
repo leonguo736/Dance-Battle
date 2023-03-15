75dfb280a7939a488155223453f69e5d356330aa
Haxrox <56497846+Haxrox@users.noreply.github.com>
2023-03-14 17:11:34 -0700

    added uart command to send http request to worldtimeapi

--- esp-fw/uart.lua
+++ esp-fw/uart.lua
@@ -1,5 +1,18 @@
 uart.on("data", "\r", function(data) 
     print("Receive from UART", data)
     uart.write(0, data)
+
+    if (string.find(data, "start")) then
+        -- ws:send("start")
+        uart.write(0, "start")
+    elseif (string.find(data, "test")) then
+        http.get("http://worldtimeapi.org/api/timezone/America/Vancouver", nil, function(code, data)
+            if (code < 0) then
+                print("HTTP request failed")
+            else
+                print(code, data)
+            end
+        end)
+    end
 end, 0)
 
