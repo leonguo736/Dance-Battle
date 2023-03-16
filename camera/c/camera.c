626f47a6c12b8e1cf6e1ed184a10f2907eea365a
Kerry Wang <kerrywang369@gmail.com>
2023-03-15 17:11:29 -0700

    removed audio code

--- camera/c/camera.c
+++ camera/c/camera.c
@@ -12,42 +12,10 @@
 #include <intelfpgaup/audio.h>
 #include <intelfpgaup/HEX.h>
 
-#define AUDIO_RATE 8000
-#define VOLUME 0x7FFFFFF
-
-#define D5SHARP 622.25
-#define F5 698.46
-
-double C_SCALE[8] = {261.63,293.66,329.63,349.23,392.00,440.00,493.88,523.25};
-
-#define PLUM 0x3947 // 7,10,7
-#define PLUM_DARK 0x18a3 // 3,5,3
-#define PLUM_LIGHT 0x59eb // 11,15,11
-
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
-#define BORDER_PADDING 10
-
-#define SONGLIST_TOP 20
-#define SONGLIST_LEFT 20
-#define SONGLIST_WIDTH 50
-#define SONGLIST_HEIGHT 25
-#define SONGLIST_ELEMENTS 4
-
-#define SONGLIST_COLOR1 0xc638
-#define SONGLIST_COLOR2 0xffff
-#define SONGLIST_FONTCOLOR 0x18e2
+const int SCREEN_WIDTH = 320;
+const int SCREEN_HEIGHT = 240;
 
 int screenBuffNum = 0;
-
-double angle = 0;
-double lastAngles[2] = {0, 0};
-double angleVelocity = 0;
-
-char * songNames[8] = {"Song A", "Song B", "Song C", "Song D", "Song E", "Song F", "Never Gonna Give You Up"};
-short songColors[8] = {video_RED, video_ORANGE, video_YELLOW, video_GREEN, video_BLUE, video_MAGENTA, video_PINK, video_GREY};
-int songSelection = 0;
-int songTop = 0;
 
 void switchScreen(void) {
     video_show();
@@ -65,64 +33,14 @@ void sketchCircle(double centerX, double centerY, int radius, short colour) {
     }
 }
 
-void drawBackground(void) {
-    for (int x = 0; x < SCREEN_WIDTH; x++) {
-        for (int y = 0; y < SCREEN_HEIGHT; y++) {
-            video_pixel(x, y, PLUM);
-        }
-    }
-    switchScreen();
-}
-
-void drawHand() {
-    video_line(200, 120, 200 + 80 * cos(lastAngles[screenBuffNum]), 120 + 80 * sin(lastAngles[screenBuffNum]), PLUM);
-    sketchCircle(200 + 80 * cos(lastAngles[screenBuffNum]), 120 + 80 * sin(lastAngles[screenBuffNum]), 5, PLUM);
-    video_line(200, 120, 200 + 80 * cos(angle), 120 + 80 * sin(angle), video_BLUE);
-    sketchCircle(200 + 80 * cos(angle), 120 + 80 * sin(angle), 5, video_BLUE);
-    lastAngles[screenBuffNum] = angle;
-    switchScreen();
-}
-
-void drawSongList() {
-    for (int i = 0; i < SONGLIST_ELEMENTS; i++) {
-        short color = i + songTop == songSelection ? SONGLIST_COLOR2 : SONGLIST_COLOR1;
-        video_box(SONGLIST_LEFT, SONGLIST_TOP + SONGLIST_HEIGHT * i, SONGLIST_LEFT + SONGLIST_WIDTH, SONGLIST_TOP + SONGLIST_HEIGHT * (i + 1), color);
-        video_box(SONGLIST_LEFT + 5, SONGLIST_TOP + SONGLIST_HEIGHT * i + 5, SONGLIST_LEFT + SONGLIST_WIDTH - 5, SONGLIST_TOP + SONGLIST_HEIGHT * (i + 1) - 5, songColors[i + songTop]);
-    }
-    switchScreen();
-}
-
-void writeAudio(int sample) {
-    audio_wait_write();
-    audio_write_left(sample);
-    audio_write_right(sample);
-}
-
-void playTone(double frequency, double duration) {
-    int sampleCount = (int)(duration * AUDIO_RATE);
-    int sampleArray[sampleCount];
-    for (int s = 0; s < sampleCount; s++) {
-        sampleArray[s] = (int)(VOLUME * sin(s * M_PI * frequency / AUDIO_RATE));
-    }
-    for (int s = 0; s < sampleCount; s++) {
-        writeAudio(sampleArray[s]);
-    }
-}
-
 void * vgaThread(void *vargp) {
-    while (1) {
-        angle += angleVelocity;
-        drawHand();
-        //drawSongList();
-        sleep(1 / 60.0);
-    }
-}
-
-void * audioThread(void *vargp) {
-    for (int tone = 0; tone < 8; tone++) {
-        playTone(C_SCALE[tone], 0.5);
-    }
-    while (1);
+    // while (1) {
+    //     angle += angleVelocity;
+    //     drawHand();
+    //     //drawSongList();
+    //     sleep(1 / 60.0);
+    // }
+    return NULL;
 }
 
 typedef struct Image {
@@ -141,7 +59,7 @@ Image read_bmp(const char* filename) {
     height = *(int*)&header[22];
     bit_depth = *(int*)&header[28];
     
-    if (height != (int)SCREEN_HEIGHT || width != (int)SCREEN_WIDTH) {
+    if (height != SCREEN_HEIGHT || width != SCREEN_WIDTH) {
         printf("Warning: img size != screen size, display middle w/ padding\n");
     }
 
@@ -234,8 +152,8 @@ void filterbyHue(Image image, int hsv_low, int hsv_high) {
 }
 
 void drawImage(Image image) {
-    int scnHeight = (int)SCREEN_HEIGHT;
-    int scnWidth = (int)SCREEN_WIDTH;
+    int scnHeight = SCREEN_HEIGHT;
+    int scnWidth = SCREEN_WIDTH;
 
     int top = max(0, (image.height - scnHeight) / 2);
     int left = max(0, (image.width - scnWidth) / 2);
@@ -257,8 +175,8 @@ void drawImage(Image image) {
 }
 
 void drawImageFiltered(Image image, int (*localFilter)(unsigned char *, int *, int), int *args, int argCount) {
-    int scnHeight = (int)SCREEN_HEIGHT;
-    int scnWidth = (int)SCREEN_WIDTH;
+    int scnHeight = SCREEN_HEIGHT;
+    int scnWidth = SCREEN_WIDTH;
 
     int top = max(0, (image.height - scnHeight) / 2);
     int left = max(0, (image.width - scnWidth) / 2);
@@ -338,7 +256,7 @@ int main(void) {
         end_time = clock();
         printf("Time taken: %fms\n", (double)(end_time - start_time) / CLOCKS_PER_SEC * 1000);
 
-        sleep(2);
+        sleep(1);
 
         start_time = clock();
         // drawImage(barackObamaFiltered);
@@ -346,7 +264,7 @@ int main(void) {
         end_time = clock();
         printf("Time taken: %fms\n", (double)(end_time - start_time) / CLOCKS_PER_SEC * 1000);
 
-        sleep(2);
+        sleep(1);
     }
 
     video_close();
