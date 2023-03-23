edf5351d3c7cfc33a00f7aec8bdb08cc715188ca
Haxrox <56497846+Haxrox@users.noreply.github.com>
2023-03-23 13:11:33 -0700

    added conditional includes

--- gui/tests/final/main.c
+++ gui/tests/final/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <pthread.h>
-// #include <intelfpgaup/video.h>
-// #include <intelfpgaup/KEY.h>
-// #include <intelfpgaup/SW.h>
-// #include <intelfpgaup/audio.h>
-
-#include "video.h"
+#ifdef GCC
 #include "KEY.h"
 #include "SW.H"
 #include "audio.h"
+#include "video.h"
+#else
+#include <intelfpgaup/KEY.h>
+#include <intelfpgaup/SW.h>
+#include <intelfpgaup/audio.h>
+#include <intelfpgaup/video.h>
+#endif
 
 #include "fonts.h"
 #include "song.h"
