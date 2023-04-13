885f1c77932a7db6560a9f2ba50e5be3d9e2de0d
Kerry Wang <kerrywang369@gmail.com>
2023-04-13 15:21:43 -0700

    demo .sof and software

--- DE1_SOC_D8M_RTL/software/slave_de1/camera.h
+++ DE1_SOC_D8M_RTL/software/slave_de1/camera.h
@@ -10,7 +10,7 @@ void writeThresholds(unsigned int i, uint8_t cbLow, uint8_t cbHigh, uint8_t crLo
  */
 void writeDeviceNumber(uint8_t deviceNumber); 
 
-#define CAMERA_HW_MODULES 6
+#define CAMERA_HW_MODULES 3
 #define CAMERA_NUM_DETECTORS 6 // if CAMERA_NUM_DETECRORS > CAMERA_HW_MODULES, then the coords will be random
 #define CAMERA_BUFFER_SIZE 3
 #define CAMERA_DIMENSIONS 2
